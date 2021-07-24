008a140758e39c3e1fef2ea928f27b7908ef96cb
shalini <shalini.v@tvsnext.io>
2021-07-24 18:37:12 +0530

    [shalini]initial commit

---
+++ PersonDataManagement/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonDataManagement
+{
+    public class Person
+    {
+        //Getter and setter for fields
+        public int SSN { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public int age { get; set; }
+
+    }
+}
---
+++ PersonDataManagement/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonDataManagement
+{
+    class Program
+    {
+      
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to Person Data Management!!!");
+                List<Person> list = new List<Person>();
+                AddPerson(list);
+
+            }
+            /// <summary>
+            /// UC1-Adding Person details and display it. 
+            /// </summary>
+            /// <param name="list"></param>
+            public static void AddPerson(List<Person> list)
+            {
+                try
+                {
+                    if (list.Count >= 0)
+                    {
+                        list.Add(new Person() { SSN = 1, name = "Shalini", address = "Chennai", age = 21 });
+                        list.Add(new Person() { SSN = 2, name = "Jay", address = "Bangalore", age = 27 });
+                        list.Add(new Person() { SSN = 3, name = "Arun", address = "Pondicherry", age = 35 });
+                        list.Add(new Person() { SSN = 4, name = "Magesh", address = "Madurai", age = 37 });
+                        list.Add(new Person() { SSN = 5, name = "Raksha", address = "Hyderabad", age = 41 });
+                    }
+                    else if (list.Count == 0)
+                    {
+                        Console.WriteLine("No details are added");
+                    }
+
+                    foreach (Person person in list)
+                    {
+                        Console.WriteLine("SSN : {0} Name : {1} Address : {2} Age : {3}  ", person.SSN, person.name, person.address, person.age);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        
+    }
+}
